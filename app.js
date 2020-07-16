fdc3bbb0bf3f38ef9b9f85cd25642352eb553860
darshnablah <dwright758@gmail.com>
2020-07-16 12:10:39 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(portfolioData);
 
-// const printProfileData = profileDataArr => {
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+  
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw new Error(err);
+  
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
+
+  // const printProfileData = profileDataArr => {
 //     for (let i = 0; i < profileDataArr.length; i++) {
 //         console.log(profileDataArr[i]);
 //     }
@@ -13,11 +33,3 @@ const [name, github] = profileDataArgs;
 // };
 
 // printProfileData(profileDataArgs);
-
-
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
